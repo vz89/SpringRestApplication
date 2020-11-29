9498a72ca6f8b199adf3cc35870302ae2b2b5dc7
Vladimir <vzlobin2003@yandex.ru>
2020-11-29 17:04:10 +0300

    Добавлена сущность Account

--- src/main/java/com/vz89/hometask/model/Account.java
+++ src/main/java/com/vz89/hometask/model/Account.java
@@ -1,5 +1,19 @@
 package com.vz89.hometask.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account")
+@Data
+@NoArgsConstructor
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_data")
+    private String accountData;
 
 }
