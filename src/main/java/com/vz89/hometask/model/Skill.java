b1b9cb9cfd79a7c289dd479e984e2e7a126b5874
Vladimir <vzlobin2003@yandex.ru>
2020-11-29 17:19:40 +0300

    Добавлена сущность Skill

--- src/main/java/com/vz89/hometask/model/Skill.java
+++ src/main/java/com/vz89/hometask/model/Skill.java
@@ -1,4 +1,18 @@
 package com.vz89.hometask.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "skill")
 public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "skill_name")
+    private String name;
 }
