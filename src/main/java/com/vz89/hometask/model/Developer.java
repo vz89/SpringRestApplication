a0e2eec915be8339b0dc644d32d6bb5f994865b5
Vladimir <vzlobin2003@yandex.ru>
2020-11-29 17:25:24 +0300

    Добавлена сущность Developer

--- src/main/java/com/vz89/hometask/model/Developer.java
+++ src/main/java/com/vz89/hometask/model/Developer.java
@@ -1,4 +1,39 @@
 package com.vz89.hometask.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "developer")
 public class Developer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "speciality")
+    private String speciality;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "developer_skill",
+            joinColumns = {@JoinColumn(name = "developer_id")},
+            inverseJoinColumns = {@JoinColumn(name = "skill_id")}
+    )
+    private Set<Skill> skills;
 }
