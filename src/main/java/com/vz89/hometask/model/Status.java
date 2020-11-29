38c782ab1e0cf40b6dbfbf749206cd2934980d00
Vladimir <vzlobin2003@yandex.ru>
2020-11-29 17:44:56 +0300

    Добавлена сущности User, Status, Role

---
+++ src/main/java/com/vz89/hometask/model/Status.java
@@ -0,0 +1,5 @@
+package com.vz89.hometask.model;
+
+public enum Status {
+    ACTIVE, APPROVAL_REQUIRED, DELETED, BANNED
+}
--- src/main/java/com/vz89/hometask/model/User.java
+++ src/main/java/com/vz89/hometask/model/User.java
@@ -1,4 +1,47 @@
 package com.vz89.hometask.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "created", columnDefinition = "DATE")
+    private LocalDate created;
+
+    @Column(name = "updated", columnDefinition = "DATE")
+    private LocalDate updated;
+
+    @Column(name = "last_password_change_date", columnDefinition = "DATE")
+    private LocalDate lastPasswordChangeDate;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name ="user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
 }
